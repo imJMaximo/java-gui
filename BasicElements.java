6c3cbf50f522413bb689bf47856ca22a862f64b8
imJMaximo <agliam1820407@mkt.ceu.edu.ph>
2019-01-08 13:22:58 +0800

    add button

--- BasicElements.java
+++ BasicElements.java
@@ -1,13 +1,19 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class BasicElements{
 
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame();
+		JButton myButton = new JButton();
+		myFrame.setLayout(null);
+		
+		myFrame.add(myButton);
+		myButton.setSize(200, 50);
+
 		myFrame.setVisible(true);
 		myFrame.setSize(900, 600);
 		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
-
+		
 		System.out.println("basic java elements");
 	}
 }
